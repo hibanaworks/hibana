677ea83764513b9c9e1b2e144d02b0515fbcca92
Hayato Ohhashi <o8@vmm.dev>
2026-06-14 21:34:44 +0900

    Refine offer alignment model hygiene

--- src/endpoint/kernel/offer/select_alignment/model.rs
+++ src/endpoint/kernel/offer/select_alignment/model.rs
@@ -1,12 +1,13 @@
 mod entry;
 mod pool;
+mod selection;
 mod set;
 
 pub(super) use self::entry::{
     CurrentOfferAuthority, CurrentOfferEntry, OfferAlignmentCandidateInput, ProgressSiblingPresence,
 };
-pub(super) use self::pool::{
-    CurrentOfferCandidateStatus, CurrentOfferObservation, OfferAlignmentCandidatePool,
-    OfferAlignmentSelection, ProgressEvidence,
+pub(super) use self::pool::OfferAlignmentCandidatePool;
+pub(super) use self::selection::{
+    CurrentOfferCandidateStatus, CurrentOfferObservation, OfferAlignmentSelection, ProgressEvidence,
 };
 pub(super) use self::set::OfferEntrySet;
